6853ef606cf5bd42b9721b1439e4480e6ab7597d
Dustin Gregory <dustingregory@users.noreply.github.com>
2019-10-26 21:58:37 -0500

    Initial modifications after fork

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,26 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+#git pull origin master;
 
 function doIt() {
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude "brew.sh" \
 		-avh --no-perms . ~;
+	PFILE=~/Library/LaunchAgents/com.__username__.homebrewupdate.plist
+	sed -i '' "s/__username__/$LOGNAME/g" ${PFILE} 
+	mv "${PFILE}" "${PFILE//__username__/$LOGNAME}"
+	/bin/launchctl load ${PFILE//__username__/$LOGNAME} 
+	/bin/launchctl list |grep $LOGNAME 
 	source ~/.bash_profile;
+        
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
--- brew.sh
+++ brew.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -13,89 +15,128 @@ BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+#brew install coreutils
+#ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+#brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+#brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+#brew install gnu-sed --with-default-names
+# Install Bash 4.
+#brew install bash
+#brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
+#if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+#  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+#  chsh -s "${BREW_PREFIX}/bin/bash";
+#fi;
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget 
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
+brew install npm
+
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
+brew install vim 
+#brew install grep
+#brew install openssh
 brew install screen
-brew install php
-brew install gmp
+#brew install php
+#brew install gmp
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+#brew tap bramstein/webfonttools
+#brew install sfnt2woff
+#brew install sfnt2woff-zopfli
+#brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+#brew install aircrack-ng
+#brew install bfg
+#brew install binutils
+#brew install binwalk
+#brew install cifer
+#brew install dex2jar
+#brew install dns2tcp
+#brew install fcrackzip
+#brew install foremost
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+#brew install pngcheck
+#brew install socat
+#brew install sqlmap
+#brew install tcpflow
+#brew install tcpreplay
+#brew install tcptrace
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install xpdf
+#brew install xz
 
 # Install other useful binaries.
-brew install ack
+#brew install ack
 #brew install exiv2
 brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
+#brew install git-lfs
+#brew install imagemagick --with-webp
+#brew install lua
+#brew install lynx
 brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install rlwrap
+#brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+#brew install vbindiff
+#brew install zopfli
+brew install htop
+brew install tmux
+brew install pstree
+
+brew install calc
+brew install cask
+brew install git
+brew install htop
+brew install p7zip
+brew install parallel
+brew install perl
+brew install python
+brew install python@2
+brew install speedtest-cli
+brew install unrar
+brew install watch
+brew install youtube-dl
+
+brew cask install iterm2
+brew cask install google-chrome
+
+brew tap homebrew/cask-fonts
+brew cask install font-meslo-nerd-font
+brew cask install visual-studio-cod
+brew cask install dotnet-sdk
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+# Set the iterm2 and terminal font to nerdfonts
+/usr/libexec/PlistBuddy -c 'Set :"New Bookmarks":0:"Normal Font" "MesloLGSNerdFontComplete-Regular 12"' ~/Library/Preferences/com.googlecode.iterm2.plist
+
+/usr/bin/osascript <<END
+tell application "Terminal"
+        set ProfilesNames to name of every settings set
+        repeat with ProfileName in ProfilesNames
+                set font name of settings set ProfileName to "MesloLGS Nerd Font"
+                set font size of settings set ProfileName to 11
+        end repeat
+end tell
+END
